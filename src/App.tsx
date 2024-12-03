f53a51b7680592e429df9e1a8eb16e5ac571b393
ernesto-ca <ernestocabanasdev@gmail.com>
2024-12-03 02:48:42 -0600

    selecting recent created note for testing and mvp, handling selected text more efficient

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,29 @@
+import { useEffect, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import ZustandHydration from '@geminotes/atoms/ZustandHydration';
 import DefaultTheme from '@geminotes/themes/DefaultTheme';
 import GlobalStyle from '@geminotes/themes/GlobalStyles';
 import GeminoteContainer from '@geminotes/atoms/GeminoteContainer';
-import GeminoteEditor from '@geminotes/organisms/GeminoteEditor';
 import GeminoteMainToolbar from '@geminotes/organisms/GeminoteMainToolbar';
 import GeminoteTypography from '@geminotes/atoms/GeminoteTypography';
 import GeminoteButton from '@geminotes/atoms/GeminoteButton';
 import useGeminotes from './stores/useGeminotes';
 import useApi from './stores/useApi';
 import useContent from './stores/useContent';
-import { useEffect, useState } from 'react';
 
 function App() {
-    const [currentSelectedText, setCurrentSelectedText] = useState('');
     const [currentNoteId, setCurrentNoteId] = useState('');
 
     const handleNoteId = (noteId: string) => {
         setCurrentNoteId(noteId);
     };
     const { summarize, summary, extractKeyPoints, keyPoints } = useApi();
-    const { notes, addNote } = useGeminotes();
+    const { editNote, currentNote } = useGeminotes();
     const { currentTabId, getTabId, executeScript } = useContent();
     // Get Tab Id for the current content store
     getTabId();
-    const onCreateNewNote = async () => {
+    const onSelectTextForNote = async () => {
         executeScript(() => {
             const iconUrl = chrome.runtime.getURL(
                 '/assets/images/cursor-pointer.png'
@@ -39,18 +37,19 @@ function App() {
                 message: currentTabId,
             },
             (selectedText) => {
-                setCurrentSelectedText(selectedText);
+                // use the recent created one or the selected one
+                if (currentNote) {
+                    editNote(currentNote.id, {
+                        content: [selectedText],
+                    });
+                }
             }
         );
     };
 
     useEffect(() => {
-        if (currentSelectedText != '') {
-            addNote('', [currentSelectedText], [], {});
-        }
-    }, [currentSelectedText]);
-
-    const selectedNote = notes.find((note) => note.id === currentNoteId);
+        onSelectTextForNote();
+    }, [currentNote]);
 
     return (
         <ZustandHydration>
@@ -60,17 +59,6 @@ function App() {
                     {/* Renderiza el toolbar si no hay nota seleccionada */}
                     {!currentNoteId && (
                         <GeminoteMainToolbar onNoteId={handleNoteId} />
-                    )}
-
-                    {/* Renderiza el editor si hay una nota seleccionada */}
-                    {currentNoteId && selectedNote && (
-                        <GeminoteEditor
-                            id={selectedNote.id}
-                            title={selectedNote.title}
-                            tags={selectedNote.tags}
-                            content={selectedNote.content}
-                            updatedAt={selectedNote.updatedAt}
-                        />
                     )}
                 </GeminoteContainer>
                 <GeminoteButton onClick={() => summarize()}>
