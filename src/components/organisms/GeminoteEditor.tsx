88afe04fb8a44aeb8cea9117351c75dc57f463bb
emmanuel ramirez <maneroto@hotmail.com>
2024-12-04 00:59:42 -0600

    Start working in references

--- src/components/organisms/GeminoteEditor.tsx
+++ src/components/organisms/GeminoteEditor.tsx
@@ -6,18 +6,24 @@ import GeminoteEditableTypography from '@geminotes/molecules/GeminoteEditableTyp
 import GeminoteEditableTag from '@geminotes/molecules/GeminoteEditableTag';
 import GeminoteTagsInput from '@geminotes/molecules/GeminoteTagsInput';
 import GeminotePaper from '@geminotes/atoms/GeminotePaper';
-import { GeminoteProps } from '@geminotes/props';
-// import { NEW_LINE, SOURCE_REFERENCE } from '@geminotes/utils/validation';
 import getFormattedDate from '@geminotes/utils/getFormattedDate';
 import GeminoteEditorNavbar from '@geminotes/molecules/GeminoteEditorNavbar';
+import GeminoteReference from './GeminoteReference';
 import GeminoteButton from '@geminotes/atoms/GeminoteButton';
 import useApi from '@geminotes/stores/useApi';
+import { GeminoteProps } from '@geminotes/props';
+import { SOURCE_REFERENCE } from '@geminotes/utils/validation';
 
 import useGeminotes from '@geminotes/stores/useGeminotes';
 
 interface GeminoteEditorProps extends Omit<GeminoteProps, 'createdAt'> {
     onOpenMenu?: () => void;
     onAddNote?: () => void;
+}
+
+interface EditableContentProps {
+    id: `${string}-${string}-${string}-${string}-${string}`;
+    text: string;
 }
 
 const StyledWrapper = styled.div`
@@ -55,10 +61,39 @@ const StyledButtonsWrapper = styled.div`
     margin-top: ${({ theme }) => theme.spacing(3)};
 `;
 
+const destructuredContent = (content: string[] | undefined) => {
+    if (
+        !content ||
+        content.length === 0 ||
+        (content.length === 1 && content[0] === '')
+    ) {
+        return undefined;
+    }
+    const newContent: EditableContentProps[] = [];
+
+    content.forEach((text) => {
+        const match = text.match(SOURCE_REFERENCE);
+        if (match) {
+            const [fullMatch] = match;
+            newContent.push({
+                id: crypto.randomUUID(),
+                text: text.replace(fullMatch, '').trim(),
+            });
+            newContent.push({
+                id: crypto.randomUUID(),
+                text: fullMatch.trim(),
+            });
+        } else newContent.push({ id: crypto.randomUUID(), text: text.trim() });
+    });
+
+    return newContent;
+};
+
 const GeminoteEditor = ({
     id,
     title,
     tags,
+    sources,
     content,
     updatedAt,
     onOpenMenu,
@@ -67,25 +102,26 @@ const GeminoteEditor = ({
     const [editableTitle, setEditableTitle] = useState<string>(
         title || 'Untitled'
     );
-    const [editableContent, setEditableContent] = useState<string[]>(
-        !content ||
-            content.length === 0 ||
-            (content.length === 1 && content[0] === '')
-            ? ['Start typing...']
-            : content
+    const [editableContent, setEditableContent] = useState<
+        EditableContentProps[]
+    >(
+        destructuredContent(content) || [
+            { id: crypto.randomUUID(), text: 'Start typing...' },
+        ]
     );
 
     useEffect(() => {
-        console.log(
-            !content ||
-                content.length === 0 ||
-                (content.length === 1 && content[0] === '')
-                ? 'start typing'
-                : content
-        );
-    }, [content]);
+        if (editableContent.length === 0) {
+            setEditableContent([
+                { id: crypto.randomUUID(), text: 'Start typing...' },
+            ]);
+        }
+    }, [editableContent]);
 
     const [editableTags, setEditableTags] = useState<string[]>(tags || []);
+    const [editableSources, setEditableSources] = useState<
+        Record<string, unknown>
+    >(sources || {});
 
     const { editNote } = useGeminotes();
 
@@ -99,14 +135,10 @@ const GeminoteEditor = ({
         handleTitleChange(newTitle);
     };
 
-    const handleContentChange = (index: number, newText: string) => {
-        const updatedContent = [...editableContent];
-        updatedContent[index] = newText;
-        setEditableContent(updatedContent);
-    };
-
     const handleContentEdit = (index: number, newText: string) => {
-        handleContentChange(index, newText);
+        const updatedContent = [...editableContent];
+        updatedContent[index] = { ...updatedContent[index], text: newText };
+        setEditableContent(updatedContent);
     };
 
     const handleTagAddition = (newTag: string) => {
@@ -136,7 +168,7 @@ const GeminoteEditor = ({
     const handleSave = () => {
         editNote(id, {
             title: editableTitle,
-            content: editableContent,
+            content: editableContent.map((line) => line.text),
             tags: editableTags,
         });
     };
@@ -145,6 +177,71 @@ const GeminoteEditor = ({
         if (onAddNote) {
             onAddNote();
         }
+    };
+
+    const UnwrapComponents = (content: EditableContentProps[]) => {
+        if (!content) return null;
+        return content.map((line, index) =>
+            line.text.match(SOURCE_REFERENCE) ? (
+                <GeminoteReference
+                    key={
+                        Object.entries(editableSources).find(
+                            ([_, value]) =>
+                                value ===
+                                parseInt(
+                                    line.text.match(SOURCE_REFERENCE)?.[1] ??
+                                        '0',
+                                    10
+                                )
+                        )?.[0]
+                    }
+                    url={
+                        Object.entries(editableSources).find(
+                            ([_, value]) =>
+                                value ===
+                                parseInt(
+                                    line.text.match(SOURCE_REFERENCE)?.[1] ??
+                                        '0',
+                                    10
+                                )
+                        )?.[0]
+                    }
+                    onDelete={handleReferenceDeletion}
+                />
+            ) : (
+                <GeminoteEditableTypography
+                    name="note-content"
+                    key={line.id}
+                    index={index}
+                    onKeyDown={(event) => handleKeyDown(event, index)}
+                    onEdit={(newText) => handleContentEdit(index, newText)}
+                >
+                    {line.text}
+                </GeminoteEditableTypography>
+            )
+        );
+    };
+
+    // Detect if 'enter' is pressed, if it is, it adds a new paragraph to editableContent when required
+    const handleKeyDown = (
+        event: React.KeyboardEvent<HTMLInputElement>,
+        index: number
+    ) => {
+        if (event.key === 'Enter' && event.currentTarget.value !== '') {
+            event.preventDefault();
+            const updatedContent = [...editableContent];
+            updatedContent.splice(index + 1, 0, {
+                id: crypto.randomUUID(),
+                text: '',
+            });
+            setEditableContent(updatedContent);
+        }
+    };
+
+    const handleReferenceDeletion = (url: string) => {
+        const updatedSources = { ...editableSources };
+        delete updatedSources[url];
+        setEditableSources(updatedSources);
     };
 
     return (
@@ -194,19 +291,7 @@ const GeminoteEditor = ({
                 </StyledButtonsWrapper>
             </StyledEditorHeader>
             <StyledEditorContent>
-                {editableContent &&
-                    editableContent.map((line, index) => (
-                        <GeminoteEditableTypography
-                            name="note-content"
-                            key={index}
-                            index={index}
-                            onEdit={(newText) =>
-                                handleContentEdit(index, newText)
-                            }
-                        >
-                            {line}
-                        </GeminoteEditableTypography>
-                    ))}
+                {UnwrapComponents(editableContent)}
             </StyledEditorContent>
             <StyledEditorFooter></StyledEditorFooter>
         </StyledWrapper>
